54c021e9c0d3df06e556666d7788cd06605bfe98
1NF1N18Y <1nf1n19y@gmail.com>
2024-05-07 14:57:15 +0200

    change stuff needed for masternodes

--- src/crypto/hmac_sha256.h
+++ src/crypto/hmac_sha256.h
@@ -1,13 +1,11 @@
 // Copyright (c) 2014 The Bitcoin Core developers
-// Copyright (c) 2017-2019 The Raven Core developers
-// Copyright (c) 2020-2021 The AIPG Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef AIPG_CRYPTO_HMAC_SHA256_H
-#define AIPG_CRYPTO_HMAC_SHA256_H
+#ifndef BITCOIN_CRYPTO_HMAC_SHA256_H
+#define BITCOIN_CRYPTO_HMAC_SHA256_H
 
-#include "crypto/sha256.h"
+#include <crypto/sha256.h>
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -31,4 +29,4 @@ public:
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
 };
 
-#endif // AIPG_CRYPTO_HMAC_SHA256_H
+#endif // BITCOIN_CRYPTO_HMAC_SHA256_H
